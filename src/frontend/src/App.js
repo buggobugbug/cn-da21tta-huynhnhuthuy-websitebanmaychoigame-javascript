35b379beb54f7224e881f56ebc6876ed74573dd2
PCM <pinsherlock1412@gmail.com>
2024-11-23 21:24:59 +0700

    update giao diện trang chủ

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './component/Navbar/Navbar';
-import Homepage from './pages/Trangchu/Homepage';
-import ProductPage from './pages/Sanpham/Product';
 
-function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/sanpham" element={<ProductPage />} />
-      </Routes>
-    </Router>
-  );
-}
+const App = () => {
+  return <></>;
+};
 
 export default App;
